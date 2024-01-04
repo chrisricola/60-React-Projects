30a71efaba0f8d1b05d858754552b863c334ab53
Chris Rico <chris.r.rico@gmail.com>
2024-01-03 22:05:16 -0800

    added another use state of Code Pen code. Added a use Effect function than sets a timeout of 2 seconds. And adds the use state of html, css, and JS with a template literal.

--- CodePen/codepen/src/Editor.jsx
+++ CodePen/codepen/src/Editor.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Editor = () => {
+    const [html, setHtml] = useState("");
+    const [css, setCss] = useState("");
+    const [js, setJs] = useState("");
+    const [codepenCode, setCodepenCode] =useState("");
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setCodepenCode(`
+                <html>
+                    <style>${css}</style>
+                    <script>${js}</script>
+                    <body>${html}</body> 
+                </html>
+            `)
+        }, 200)
+
+        return () => clearTimeout(timeout)
+    }, [html, css, js])
+
     return (
         <div>
-            Yessir
+            <div>
+                <textarea value={html} type="text" onChange={(e) => {
+                    setHtml(e.target.value)
+                }} />
+                <textarea value={css} type="text" onChange={(e) => {
+                    setCss(e.target.value)
+                }} />
+                <textarea value={js} type="text" onChange={(e) => {
+                    setJs(e.target.value)
+                }} />
+            </div>
+            <div>
+                {codepenCode}
+            </div>
         </div>
+        
     )
 }
 
