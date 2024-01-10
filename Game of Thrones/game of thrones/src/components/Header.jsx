ec8c0d8f5c95b87a7d049df0f5298d9a959ddb9f
Chris Rico <chris.r.rico@gmail.com>
2024-01-09 20:47:21 -0800

    added the header to the app js file for the dom. Added the Header function and layout in header jsx file. Added a hero, banner, h1 title and link. Added styling to the Header.

---
+++ Game of Thrones/game of thrones/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div className="hero">
+            <div className="hero-banner">
+                <h1>Game of Thrones</h1>
+                <p>When people ask you what happened here, tell the m the North remembers.</p>
+                <a href="#" className='hero-btn'>Characters</a>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
