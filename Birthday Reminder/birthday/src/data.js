a29b8f3695ac7eb9e2eff01613e9ba63faa35338
Chris Rico <chris.r.rico@gmail.com>
2024-01-22 22:31:41 -0800

    added data to the data js file and exported into the Card js file. Added the get Date function. In the function, it loops thru the data for a matching day to birthday and appends to DOM

---
+++ Birthday Reminder/birthday/src/data.js
@@ -0,0 +1,70 @@
+const data = [
+
+    {
+        id: 1,
+        name: "Yuri Flurry",
+        day: 7,
+        month: 4,
+        age: 46,
+        gender: "male"
+    },
+    {
+        id: 2,
+        name: "Jerry Garcia",
+        day: 17,
+        month: 10,
+        age: 86,
+        gender: "male"
+    },
+    {
+        id: 3,
+        name: "Boni Rokit",
+        day: 22,
+        month: 5,
+        age: 29,
+        gender: "female"
+    },
+    {
+        id: 4,
+        name: "Mike Herrera",
+        day: 10,
+        month: 18,
+        age: 49,
+        gender: "male"
+    },
+    {
+        id: 5,
+        name: "Diana Smith",
+        day: 9,
+        month: 1,
+        age: 66,
+        gender: "female"
+    },
+    {
+        id: 6,
+        name: "Gloria Mora",
+        day: 24,
+        month: 3,
+        age: 33,
+        gender: "female"
+    },
+    {
+        id: 7,
+        name: "Flaco Paco",
+        day: 22,
+        month: 1,
+        age: 36,
+        gender: "male"
+    },
+    {
+        id: 8,
+        name: "Chat Vato",
+        day: 12,
+        month: 12,
+        age: 26,
+        gender: "male"
+    },
+]
+
+export default data;
+
