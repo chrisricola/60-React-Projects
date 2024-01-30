209b6d00f4ebae619db6ad753cae0b13992404c4
Chris Rico <chris.r.rico@gmail.com>
2024-01-29 17:01:12 -0800

    added the home page layout in Home jsx file. Created the input bar to seach books. Added styling to the home page with background image.

---
+++ Search Book/bookstore/src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <header>
+            <div className="hero">
+                <div className="hero-banner">
+                    <h4>Book Store</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, officiis!</p>
+                    <input type='text' placeholder='' className='input-bar'/>
+                    <button className='hero-btn'>Search</button>
+                </div>
+            </div>
+        </header>
+    </div>
+  )
+}
+
+export default Home
