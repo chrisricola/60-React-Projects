2ec5cc7abfd23c6b312ba529f3b1cc89f288cd5e
Chris Rico <chris.r.rico@gmail.com>
2024-02-05 18:06:17 -0800

    added functions from the main page to the seach page with the difference in the key for google api, added the word subject. Rest of the layout is similar to main jsx page.

--- Search Book/bookstore/src/components/Search.jsx
+++ Search Book/bookstore/src/components/Search.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 
 const Search = () => {
+  const params = useParams(); 
+  const [books, setBooks] = useState([]);
+
+    const getBooks = (name) => {
+        axios.get('https://www.googleapis.com/books/v1/volumes?q=subject' + name + '=free-ebooks&key=AIzaSyBwNNlsOxRG1CanfoEZkZtlFplVnRWAQvc&maxResults=30')
+          .then(res => setBooks(res.data.items))
+          .catch(err => console.log(err))
+      }
+    
+      useEffect(() => {
+        getBooks(params.name);
+      }, [params.name]);
   return (
-    <div>Search</div>
+    <div className='row' style={{ margin: '50px'}}>
+        {
+            books.map((item) => {
+                let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail; 
+                return (
+                    <div className="col-lg-2 col-md-3 col-sm-6">
+                        <div className='projectCard'>
+                            <img src={thumbnail} alt="" style={{marginTop: "20px", width: "120px", height:"150px"}}/>
+                            <div className="info">
+                                <h5 style={{color: "black", fontSize: "12px"}}>{item.volumeInfo.title}</h5>
+                                <p style={{color: "black", fontSize: "12px"}}>{item.volumeInfo.authors}</p>
+                                <button className='btn btn-sm btn-success' style={{marginBottom:"40px", marginTop:"0px"}}>
+                                    <a href={item.volumeInfo.previewLink} style={{textDecoration:"none",color:"white" }}>Get Book</a>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    
+                ) 
+            })
+        }
+    </div>
   )
 }
 
