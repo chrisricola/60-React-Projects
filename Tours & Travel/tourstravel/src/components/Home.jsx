44a6292f039cd55289a0c740ae3341ac8518fe0b
Chris Rico <chris.r.rico@gmail.com>
2024-02-07 22:16:54 -0800

    added bootstrap cdn links to html page. Added a main jsx file that is the card for the premium packages. It has an image, price button and a visit button. Added style to main page.

--- Tours & Travel/tourstravel/src/components/Home.jsx
+++ Tours & Travel/tourstravel/src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Hero'
+import Main from './Main'
 
 const Home = () => {
   return (
     <div>
         <Hero />
+        <Main />
     </div>
   )
 }
---
+++ Tours & Travel/tourstravel/src/components/Main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import data from '../data'
+
+
+const Main = () => {
+  return (
+    <div className='main'>
+        <h4>Our Premium Packages</h4>
+        <div className="row">
+            {
+                data.map((item) => {
+                    return (
+                        <div className=" col-lg-4 col-md-6 col-sm-12">
+                            <div className="card-body">
+                                <h5 className='card-title'>{item.name}</h5>
+                            </div>
+                            <img src={item.url} alt='' />
+                            <button className='price'>{item.day}Days: {item.price}</button>
+                            <button className='hero-btn btn-2'>Visit</button>
+                        </div>
+                    )
+                })
+            }
+        </div>
+
+    </div>
+  )
+}
+
+export default Main
