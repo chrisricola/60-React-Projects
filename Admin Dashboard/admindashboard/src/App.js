d949228c24979920f547be4de0e276fe7ef94033
Chris Rico <chris.r.rico@gmail.com>
2024-02-14 15:53:43 -0800

    installed chart js and material ui. Deleted unwanted files from react app. Deleted lines of code in app js and index js. Added a new app js function. Imported Box, creatTheme and Themprovider from material ui. Created a new useState for light and darkt theme.

--- Admin Dashboard/admindashboard/src/App.js
+++ Admin Dashboard/admindashboard/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {Box, createTheme, ThemeProvider } from '@mui/material';
 
 function App() {
+  const [mode, Setmode] = useState("light");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    }
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={darkTheme}>
+        <Box>
+          
+        </Box>
+      </ThemeProvider>
     </div>
   );
 }
