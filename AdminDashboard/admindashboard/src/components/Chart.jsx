b3926e7ce8e9d5b7092caf0cd93ab601928a6933
Chris Rico <chris.r.rico@gmail.com>
2024-02-24 16:25:00 -0800

    Added styling and layout to the card for the Bar chart. Also added a userdata 2 for user losses from data and copied card for another barchart. As well as added a card with a piechart.

--- AdminDashboard/admindashboard/src/components/Chart.jsx
+++ AdminDashboard/admindashboard/src/components/Chart.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { UserData } from '../Data';
 import Barchart from './Barchart';
+import { Card, Stack } from '@mui/material';
+import PieChart from './PieChart';
 
 const Chart = () => {
     const [userData, setUserData] = useState({
@@ -24,8 +26,50 @@ const Chart = () => {
         ],
     });
 
+    const [userData2, setUserData2] = useState({
+        labels: UserData.map((data) => data.year),
+        datasets: [
+        
+            {
+                label: "User Lost",
+                data: UserData.map((data) => data.Lost),
+                backgroundColor: [
+                    "#fff",
+                    "#ffe3a3",
+                    "#ffd166",
+                    "#ffda85",
+                    "#cca752"
+                ],
+                borderColor: "white",
+                borderWidth: 2,
+                color: "white",
+            },
+        ],
+    });
+    
+
   return (
-    <Barchart chartData={userData}/>
+    <div>
+        <Stack direction={"row"} spacing={4}>
+            <Card sx={{width: 310, backgroundColor: "#21295c"}}>
+                <div style={{width: 300, padding: 5}}>
+                <Barchart chartData={userData}/>
+                </div>
+            </Card>
+            <Card sx={{width: 310, backgroundColor: "#21295c"}}>
+                <div style={{width: 300, padding: 5}}>
+                <Barchart chartData={userData2}/>
+                </div>
+            </Card>
+            <Card sx={{width: 310, backgroundColor: "#21295c"}}>
+                <div style={{width: 300, padding: 5}}>
+                <PieChart chartData={userData}/>
+                </div>
+            </Card>
+            
+        </Stack>
+    </div>
+    
   )
 }
 
