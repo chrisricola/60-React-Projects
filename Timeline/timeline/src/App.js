7eb3ab3fb7ee17e86ca9d96e1384be9b21e632e8
Chris Rico <chris.r.rico@gmail.com>
2024-03-08 18:21:28 -0800

    added an h3 that contains the title, a h5 that contains the location and a paragraph that contains the descriptiton. Added styling icons from timeline styles from timeline package.

--- Timeline/timeline/src/App.js
+++ Timeline/timeline/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import timeLineElements from './data';
+import 'react-vertical-timeline-component/style.min.css'
 
 const App = () => {
+
+  let workIcon = {background: "limegreen"}
+  let schoolIcon = {background: "lightcoral"}
   return (
     <div>
       <h1 className='title'>Timeline</h1>
@@ -11,7 +15,11 @@ const App = () => {
           timeLineElements.map((items) => {
 
             return(
-              <VerticalTimelineElement key={items.id} date={items.date} dateClassName='date' />
+              <VerticalTimelineElement key={items.id} date={items.date} dateClassName='date'  iconStyle={items.icon === "work" ? workIcon : schoolIcon}>
+                <h3 className='vertical-timeline-element-title'>{items.title}</h3>
+                <h5 className='vertical-timeline-element-subtitle'>{items.location}</h5>
+                <p className='p'>{items.description}</p>
+              </VerticalTimelineElement>
             )
           })
         }
