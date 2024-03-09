eb9b582855ff0700dde291aa0e87447a6ab9c104
Chris Rico <chris.r.rico@gmail.com>
2024-03-08 18:04:55 -0800

    added react tmeline package. Crreated timeline layout in app js. Imported vertical timeline elements in project. Created a data js file with data of dates.

--- Timeline/timeline/src/App.js
+++ Timeline/timeline/src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import timeLineElements from './data';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1 className='title'>Timeline</h1>
+      <VerticalTimeline>
+        {
+          timeLineElements.map((items) => {
+
+            return(
+              <VerticalTimelineElement key={items.id} date={items.date} dateClassName='date' />
+            )
+          })
+        }
+      </VerticalTimeline>
+    </div>
   )
 }
 
