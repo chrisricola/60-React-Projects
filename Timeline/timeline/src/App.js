b4e92543727b71f789c56ba74b030653f091fea6
Chris Rico <chris.r.rico@gmail.com>
2024-03-11 17:41:43 -0700

    adde final styling to App js file.

--- Timeline/timeline/src/App.js
+++ Timeline/timeline/src/App.js
@@ -7,18 +7,20 @@ const App = () => {
 
   let workIcon = {background: "limegreen"}
   let schoolIcon = {background: "lightcoral"}
+  
   return (
     <div>
       <h1 className='title'>Timeline</h1>
       <VerticalTimeline>
         {
           timeLineElements.map((items) => {
-
+            let Button = items.buttonText !== undefined && items.buttonText !== null && items.buttonText !== "" 
             return(
               <VerticalTimelineElement key={items.id} date={items.date} dateClassName='date'  iconStyle={items.icon === "work" ? workIcon : schoolIcon}>
                 <h3 className='vertical-timeline-element-title'>{items.title}</h3>
                 <h5 className='vertical-timeline-element-subtitle'>{items.location}</h5>
                 <p className='p'>{items.description}</p>
+                {Button && (<button className={`btn ${items.icon === "work" ? "work-btn" : "school-btn"}`}>{items.buttonText}</button>)}
               </VerticalTimelineElement>
             )
           })
