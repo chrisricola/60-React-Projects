29622b7260985c7ad9151777329029d9e736e2c0
Chris Rico <chris.r.rico@gmail.com>
2024-03-12 22:14:13 -0700

    added layout to home jsx file. Added a title, input and paragraph. Added styling to home page. Image as background.

--- MealPlanner/mealplanner/src/components/Home.jsx
+++ MealPlanner/mealplanner/src/components/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import '../index.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <header className='hero'>
+            <div className="hero-banner">
+                <h4>Meal Planner</h4>
+                <p>Tell me what you eat, and I will tell you who you are.</p>
+                <input className='input-bar' type='text' placeholder='Press Enter' />
+            </div>
+        </header>
+    </div>
   )
 }
 
