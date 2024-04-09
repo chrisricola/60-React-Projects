b3ce571cd4ed9d1ee662cf10db4c3ef1d76ca73e
Chris Rico <chris.r.rico@gmail.com>
2024-04-08 17:46:36 -0700

    added new file Drum jsx. Added styling to background, h1, h4 and image.

--- Drumkit/drumkit/src/App.js
+++ Drumkit/drumkit/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './components/Hero'
 
 const App = () => {
+  
   return (
     <div>
       <Hero />
--- Drumkit/drumkit/src/components/Hero.jsx
+++ Drumkit/drumkit/src/components/Hero.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import crash from "../images/crash.png";
+import '../index.css';
 
 const Hero = () => {
-  return (
+  const [name, setName] = useState(crash)
+   return (
     <div >
         <div className="container">
             <h1>Drum Band</h1>
         </div>
         <h4>Press any button</h4>
-        <img src={crash} alt='' className='img' />
+        <img src={name} alt='' className='img' />
     </div>
   )
 }
