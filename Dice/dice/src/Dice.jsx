f3aa65168075b110d9c876ca78b6f3ed8f7f51f1
Chris Rico <chris.r.rico@gmail.com>
2024-04-21 20:58:56 -0700

    added to variables with function that pick a random number thru 6 and then a query selector for dice 1 and 2 to display. Added onclick to start button.

--- Dice/dice/src/Dice.jsx
+++ Dice/dice/src/Dice.jsx
@@ -3,6 +3,13 @@ import list from './list';
 
 const Dice = () => {
   let x = 0, y = 0;
+  const start = () => {
+    var number1 = Math.floor(Math.random() * 6);
+    document.querySelector('.img1').setAttribute("src", list[number1]);
+
+    var number2 = Math.floor(Math.random() * 6);
+    document.querySelector('.img2').setAttribute("src", list[number2]);
+  }
   return (
     <div className='container'>
         <div>
@@ -17,7 +24,7 @@ const Dice = () => {
           </div>
           <h4>Choose A Number</h4>
           <input type='text' placeholder='' className='input-bar' />
-          <button className='btn'>Start</button>
+          <button className='btn' onClick={() => start()}>Start</button>
           <button className='btn'>Instruction</button>
         </div>
     </div>
