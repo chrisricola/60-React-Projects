00da60aa84d928ba2aebd645d673ac3af8138708
Chris Rico <chris.r.rico@gmail.com>
2024-04-21 20:50:09 -0700

    created layout for the dice game on dice jsx file. Imported the list from file, imported images. Also imported into APP file to appear on DOM.

--- Dice/dice/src/App.js
+++ Dice/dice/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Dice from './Dice'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Dice />
+    </div>
   )
 }
 
--- Dice/dice/src/Dice.jsx
+++ Dice/dice/src/Dice.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import list from './list';
 
 const Dice = () => {
+  let x = 0, y = 0;
   return (
-    <div>
-        
+    <div className='container'>
+        <div>
+          <h1>Player 1 wins</h1>
+          <div className="dice">
+            <p>Player 1</p>
+            <img className='img1' src={list[x]} alt='' />
+          </div>
+          <div className="dice">
+            <p>Player 1</p>
+            <img className='img2' src={list[y]} alt='' />
+          </div>
+          <h4>Choose A Number</h4>
+          <input type='text' placeholder='' className='input-bar' />
+          <button className='btn'>Start</button>
+          <button className='btn'>Instruction</button>
+        </div>
     </div>
   )
 }
