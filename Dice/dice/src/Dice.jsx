ec39e6a997a8966bd1a046f6bcaf55d3dde0efa2
Chris Rico <chris.r.rico@gmail.com>
2024-04-22 23:19:26 -0700

    added a use state for the results and target. If the number of the target equlas the number put in the input, the winning player is added to the result heading.

--- Dice/dice/src/Dice.jsx
+++ Dice/dice/src/Dice.jsx
@@ -1,29 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import list from './list';
 
 const Dice = () => {
   let x = 0, y = 0;
+
+  const [target, setTarget] = useState();
+  const [result, setResult] = useState("Roll the Dice");
+
   const start = () => {
     var number1 = Math.floor(Math.random() * 6);
     document.querySelector('.img1').setAttribute("src", list[number1]);
 
     var number2 = Math.floor(Math.random() * 6);
     document.querySelector('.img2').setAttribute("src", list[number2]);
+
+    if(number1 + 1 === Number(target)) {
+      setResult("Player 1 Wins")
+    } else if(number2 + 1 === Number(target)) {
+      setResult("Player 2 Wins")
+    } else if(number1 === Number(target) && number2 === Number(target)) {
+      setResult("Draw")
+    }
   }
   return (
     <div className='container'>
         <div>
-          <h1>Player 1 wins</h1>
+          <h1>{result}</h1>
           <div className="dice">
             <p>Player 1</p>
             <img className='img1' src={list[x]} alt='' />
           </div>
           <div className="dice">
-            <p>Player 1</p>
+            <p>Player 2</p>
             <img className='img2' src={list[y]} alt='' />
           </div>
           <h4>Choose A Number</h4>
-          <input type='text' placeholder='' className='input-bar' />
+          <input type='text' placeholder='' className='input-bar' onChange={(e) => {setTarget(e.target.value)}}/>
           <button className='btn' onClick={() => start()}>Start</button>
           <button className='btn'>Instruction</button>
         </div>
