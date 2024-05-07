7cb64e29d8cf736f3d43331fb8d210d3f11f49a0
Chris Rico <chris.r.rico@gmail.com>
2024-05-06 23:06:09 -0700

    added two new buttons, a prevuious and next. Added a new useState for the offset in the api to change on a onCLick.

--- Pokemon/pokemon/src/Main.jsx
+++ Pokemon/pokemon/src/Main.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from 'react'
 
 const Main = () => {
     const [characters, setCharacters] = useState([]);
+    const [offset, setOffset] = useState(0);
     const getCharacter = async () => {
-        const api = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20");
+        const api = await fetch("https://pokeapi.co/api/v2/pokemon?offset=" + offset + "&limit=20");
 
         const data = await api.json();
 
@@ -21,10 +22,10 @@ const Main = () => {
           characters.map((item, id) =>(
             <div className='col-lg-4 col-md-6 col-sm-12'> 
             <div className="card">
-              <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id+1}.svg`} alt='' />
+              <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id+1+offset}.svg`} alt='' />
               <div className="card-body">
-                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id+1}.png`} alt='' />
-                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id+1}.png`} alt='' />
+                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id+1+offset}.png`} alt='' />
+                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id+1+offset}.png`} alt='' />
                 <h6>{item.name}</h6>
                 <button className='btn btn-success'>More</button>
               </div>
@@ -33,6 +34,8 @@ const Main = () => {
           ))
         }
       </div>
+      <button className='btn btn-success' onClick={() => setOffset(offset - 20)}>Prev</button>
+      <button className='btn btn-success' onClick={() => setOffset(offset + 20)}>Next</button>
     </div>
   )
 }
