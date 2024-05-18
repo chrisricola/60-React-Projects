4e0a660107ff1181306dff7dd19c2f8883df386f
Chris Rico <chris.r.rico@gmail.com>
2024-05-18 15:42:55 -0700

    imported Home into app js file. Added a new file called list that exports the array of images imported from images files. Created layout of slot machine in Home file. Created the stack and boxs from material ui.

---
+++ SlotMachine/slotmachine/src/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {Box, Stack} from '@mui/material';
+import img from '../src/images/slot2.jpg';
+import list from './list'
+
+const Home = () => {
+  return (
+    <div>
+        <Box>
+            <Stack direction={"row"}>
+              <Box flex={2} sx={{backgroundClip: "rgb(8, 195, 161)"}} />
+              <Box flex={2} sx={{textAlign: "center"}}>
+                <Stack direction={"column"} >
+                  <Box sx={{postion:"relative"}}>
+                    <img src={img} alt="" />
+                    <img src={list[0]} alt="" />
+                    <img src={list[0]} />
+                    <img src={list[0]} alt="" />
+                  </Box>
+                </Stack>
+              </Box>
+              <Box flex={2} sx={{backgroundClip: "rgb(8, 195, 161)"}} />
+            </Stack>
+        </Box>
+    </div>
+  )
+}
+
+export default Home
---
+++ SlotMachine/slotmachine/src/list.js
@@ -0,0 +1,10 @@
+import apple from './images/Apple.jpg';
+import banana from './images/Banana.jpg';
+import grapes from './images/Grapes.jpg';
+import mango from './images/Mango.jpg';
+import papaya from './images/Papaya.jpg';
+import pineapple from './images/Pineapple.jpg';
+
+const list = [apple, banana, grapes, mango, papaya, pineapple];
+
+export default list;
