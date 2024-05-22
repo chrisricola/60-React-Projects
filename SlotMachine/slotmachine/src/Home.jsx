a10b6047cebabec41014b4cb8e6c516fdc24eb3c
Chris Rico <chris.r.rico@gmail.com>
2024-05-21 17:14:34 -0700

    has issues with Box and Stack in the home js file. Fixed issue. Installed Material UI again. Added styles to images, buttons and layout.

--- SlotMachine/slotmachine/src/Home.jsx
+++ SlotMachine/slotmachine/src/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Box, Stack} from '@mui/material';
+import { Box, Stack } from '@mui/material';
 import img from '../src/images/slot2.jpg';
 import list from './list';
 import './index.css';
@@ -10,22 +10,28 @@ const Home = () => {
         <Box>
             <Stack direction={"row"}>
               <Box flex={2} sx={{backgroundClip: "rgb(8, 195, 161)"}} />
-              <Box flex={3} sx={{textAlign: "center"}}>
+
+              <Box fflex={3} sx={{ textAlign: "center" }}>
                 <Stack direction={"column"} >
-                  <Box sx={{postion:"relative"}}>
-                    <img className='bc' src={img} alt="" />
-                    <img className='img-1' src={list[0]} alt="" />
-                    <img className='img-2' src={list[0]} />
-                    <img className='img-3' src={list[0]} alt="" />
+                  <Box sx={{ position: "relative" }}>
+                    <img className='bc' src={img} alt='' />
+                    <img className='img-1' src={list[0]} alt=''  />
+                    <img className='img-2' src={list[0]} alt='' />
+                    <img className='img-3' src={list[0]} alt='' />
                   </Box>
-                  <Box sx={{postion:"relative"}}>
+                  <Box sx={{ position: "relative" }}>
                     <span className='dollar'>$</span>
-                    <input type='text' placeholder='' className='input'/>
-                  </Box> 
+                    <input type='text' placeholder='Amount' className='input'
+                     />
+                  </Box>
+
                   <button className='btn'>Start</button>
+
                 </Stack>
               </Box>
+
               <Box flex={2} sx={{backgroundClip: "rgb(8, 195, 161)"}} />
+
             </Stack>
         </Box>
     </div>
