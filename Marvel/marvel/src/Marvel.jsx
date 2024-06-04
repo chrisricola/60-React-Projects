6a5564de58415d108c984e200e6b1dfb4074bbd9
Chris Rico <chris.r.rico@gmail.com>
2024-06-03 22:22:45 -0700

    created the layout for the character card from API. Used bootstrap classname's for layout. Added img, character name and button.

--- Marvel/marvel/src/Marvel.jsx
+++ Marvel/marvel/src/Marvel.jsx
@@ -4,7 +4,7 @@ const Marvel = () => {
     const [characters, setCharaters] = useState([]);
 
     const getCharacter = async () => {
-        const api = await fetch("https://gateway.marvel.com/v1/public/characters?nameStartsWith=spider&ts=1&apikey=60c5976a669d2106e5bf80446a9db36e&hash=6d23d1f44afe66c5e086c31958a0d845");
+        const api = await fetch("https://gateway.marvel.com/v1/public/characters?nameStartsWith=iron&ts=1&apikey=60c5976a669d2106e5bf80446a9db36e&hash=6d23d1f44afe66c5e086c31958a0d845");
         const data = await api.json();
         setCharaters(data.data.results)
         console.log(data.data.results)
@@ -14,7 +14,23 @@ const Marvel = () => {
         getCharacter();
     }, [])
   return (
-    <div>Marvel</div>
+    <div className='main'>
+      <div className="row">
+        {
+          characters.map((item) => (
+            <div className='col-lg-3 com-md-6'>
+                <div className="card">
+                  <img src={item.thumbnail.path + "." + item.thumbnail.extension} alt='' className='card-image-top' />
+                  <h4 className='card-header'>{item.name}</h4>
+                  <div className="card-body">
+                    <button className='btn btn-danger'>View</button>
+                  </div>
+                </div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
