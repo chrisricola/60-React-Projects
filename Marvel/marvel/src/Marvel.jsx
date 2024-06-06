fc7cf9f827dfd783a6f5b274fb606b0e6d267a72
Chris Rico <chris.r.rico@gmail.com>
2024-06-05 22:22:03 -0700

    added a new File called Hero jsx. Added to new useStates for show and hero data. Added an onclick event that pushes a function of setShow and heroData if False, then moves to the Hero component and shows info about hero.

--- Marvel/marvel/src/Marvel.jsx
+++ Marvel/marvel/src/Marvel.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from 'react'
+import Hero from './Hero';
+
 
 const Marvel = () => {
     const [characters, setCharaters] = useState([]);
+    const [show, setShow] = useState(false);
+    const [heroData, setHeroData] = useState();
 
     const getCharacter = async () => {
         const api = await fetch("https://gateway.marvel.com/v1/public/characters?nameStartsWith=iron&ts=1&apikey=60c5976a669d2106e5bf80446a9db36e&hash=6d23d1f44afe66c5e086c31958a0d845");
@@ -15,6 +19,7 @@ const Marvel = () => {
     }, [])
   return (
     <div className='main'>
+      {show !== true && 
       <div className="row">
         {
           characters.map((item) => (
@@ -23,13 +28,14 @@ const Marvel = () => {
                   <img src={item.thumbnail.path + "." + item.thumbnail.extension} alt='' className='card-image-top' />
                   <h4 className='card-header'>{item.name}</h4>
                   <div className="card-body">
-                    <button className='btn btn-danger'>View</button>
+                    <button className='btn btn-danger' onClick={() => {setShow(true); setHeroData(item)}}>View</button>
                   </div>
                 </div>
             </div>
           ))
         }
-      </div>
+      </div>}
+      {show === true && <Hero setShow={setShow} heroData={heroData}/>}
     </div>
   )
 }
