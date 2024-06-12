04a38b9ffde1ba7790794f1ddd5b95393755f525
Chris Rico <chris.r.rico@gmail.com>
2024-06-11 18:44:08 -0700

    added bootstrap link to html. Added card lay out using bootsrap with Image and button.

--- MemeGenerator/meme/src/GetMeme.jsx
+++ MemeGenerator/meme/src/GetMeme.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 
 const GetMeme = () => {
     const [meme, setMeme] = useState([]);
+    const [show, setShow] = useState(false);
+    const [memeData, setMemeData] = useState();
 
     const getMeme = async () => {
         const api = await fetch("https://meme-api.com/gimme/50")
@@ -14,8 +16,26 @@ const GetMeme = () => {
         getMeme()
     }, []);
   return (
-    <div>
-
+    <div className='get-meme'>
+      {show !== true && 
+      <div className="row">
+        {
+          meme.map((item) => (
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="card">
+                <img src={item.preview[2]} alt='' />
+                <div className="card-body">
+                  <button className='btn btn-info' onClick={() => {setShow(true), setMemeData(item)}}>View</button>
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+      }
+      {
+      show === true && <Meme set={show} memeData={memeData}/>
+    }
     </div>
   )
 }
