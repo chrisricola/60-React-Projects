31cd93eacdb973e755d3cee7402ccaa5c54dd668
Chris Rico <chris.r.rico@gmail.com>
2024-07-22 22:16:16 -0700

    added a const img with a link of an image, and pass it thru a function called setImage. Then pass it thru an onError function in the img layout.

--- News/news/src/components/Main.jsx
+++ News/news/src/components/Main.jsx
@@ -13,17 +13,24 @@ const Main = () => {
         setNews(data.articles);
         console.log(data.articles);
     }
+
+    const img = "https://c0.wallpaperflare.com/preview/105/94/569/administration-articles-bank-black-and-white.jpg";
+
+    const setImage = event => {
+      event.target.src = img;
+    }
   return (
     <div className='news'>
       <div className="row">
         {
           news.map((item) => (
-            <div className="col col-lg-4 col-md-6 col-sm-12">
+            <div className="col col-lg-4 col-md-6 col-sm-12 news-column">
               <div className="card">
-                <img src={item.urlToImage} alt="" className='card-img-top'/>
+
+                <img src={item.urlToImage} alt="" className='card-img-top' onError={setImage}/>
                 <h6 className="card-header">{item.title}</h6>
                 <div className="card-body">
-                  <p>{item.description}</p>
+                  <p className='news-text'>{item.description}</p>
                   <button className='btn btn-success'>
                     <a href={item.url}>More</a>
                   </button>
