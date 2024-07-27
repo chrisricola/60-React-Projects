a8a00f6d534a91fbc7a48a710049b9977916e6b9
Chris Rico <chris.r.rico@gmail.com>
2024-07-26 23:20:29 -0700

    added an onClick event to the buttons in Category file. in the OnClick, the setSearch state will search for the category of the button.

--- News/news/src/components/Catergory.jsx
+++ News/news/src/components/Catergory.jsx
@@ -5,14 +5,15 @@ const Catergory = ({setSearch}) => {
     <div>
     <div className="category">
         <ul>
-            <li><button className="btn btn-outline-success">Sports</button></li>
-            <li><button className="btn btn-outline-danger">Health</button></li>
-            <li><button className="btn btn-outline-primary">Entertainment</button></li>
+            <li><button className="btn btn-outline-success" onClick={() => setSearch('sports')}>Sports</button></li>
+            <li><button className="btn btn-outline-danger" onClick={() => setSearch('health')}>Health</button></li>
+            <li><button className="btn btn-outline-primary" onClick={() => setSearch('entertainment')}>Entertainment</button></li>
         </ul>
+        
         <ul>
-            <li><button className="btn btn-outline-warning">Buisness</button></li>
-            <li><button className="btn btn-outline-info">Technology</button></li>
-            <li><button className="btn btn-outline-primary">Science</button></li>
+            <li><button className="btn btn-outline-warning" onClick={() => setSearch('buisness')}>Buisness</button></li>
+            <li><button className="btn btn-outline-info" onClick={() => setSearch('technology')}>Technology</button></li>
+            <li><button className="btn btn-outline-primary" onClick={() => setSearch('science')}>Science</button></li>
         </ul>
     </div>
     </div>
