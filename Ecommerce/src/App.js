389f0e4dc787990b5d9988d5d61ff4fe3fca59a0
Chris Rico <chris.r.rico@gmail.com>
2024-08-16 16:52:21 -0700

    added a new components folder with a Header js and css file. Created Header function and cut and pasted header/nav layout from app js file. Added an input search bar, a search logo and give style to header logo.

--- Ecommerce/src/App.js
+++ Ecommerce/src/App.js
@@ -1,31 +1,13 @@
 import React from 'react'
 import './App.css'
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-import StorefrontIcon from '@mui/icons-material/Storefront';
+import Header from './Components/Header';
 
 
 
 const App = () => {
   return (
     <div className='app'>
-      <div className='header__logo'>
-        <StorefrontIcon />
-      </div>
-      <div className="header__search"></div>
-      <div className="header__nav">
-        <div className="nav__item">
-          <span className="nav__itemLineOne">Hello Guest</span>
-          <span className="nav__itemLineTwo">Sign In</span>
-        </div>
-        <div className="nav__item">
-          <span className="nav__itemLineOne">Your</span>
-          <span className="nav__itemLineTwo">Shop</span>
-        </div>
-        <div className="nav__item">
-          <ShoppingBasketIcon fontSize='large'/>
-          <span className="nav__itemLineTwo">0</span>
-        </div>
-      </div>
+      <Header/>
     </div>
   )
 }
