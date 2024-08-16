faf0b7c75375822bd9354a3eb8734d6e3a286c89
Chris Rico <chris.r.rico@gmail.com>
2024-08-15 23:14:19 -0700

    imported Material UI. Created a nav bar in APP js file. Created the header for the logo, search bar and nav items such as Sign in And basket icon.

--- Ecommerce/src/App.js
+++ Ecommerce/src/App.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import './App.css'
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <div className='header__logo'></div>
+      <div className="header__search"></div>
+      <div className="header__nav">
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Hello Guest</span>
+          <span className="nav__itemLineTwo">Sign In</span>
+        </div>
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Your</span>
+          <span className="nav__itemLineTwo">Shop</span>
+        </div>
+        <div className="nav__item">
+          <ShoppingBasketIcon fontSize='large'/>
+          <span className="nav__itemLineTwo">0</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
