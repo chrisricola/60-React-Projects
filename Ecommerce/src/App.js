d5b10ddb9cfb2a2ec274ef7a86958cbdf0d7799a
Chris Rico <chris.r.rico@gmail.com>
2024-08-28 23:53:24 -0700

    In the app file, imported BowserRouter as Router, Route and Routed from react router dom. In thenapp layout, created routes for checkout that will lead to the checkout page, and well as home for the logo. In the header component, added a Link from reacter router dom that send the page to either home when logo is clicked, or checkout when basket is clicker.

--- Ecommerce/src/App.js
+++ Ecommerce/src/App.js
@@ -2,14 +2,27 @@ import React from 'react'
 import './App.css'
 import Header from './Components/Header';
 import Home from './Components/Home';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Checkout from './Components/Checkout';
 
 
 
 const App = () => {
   return (
     <div className='app'>
-      <Header/>
-      <Home />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/checkout' element={<Checkout/>}>
+            
+          </Route>
+
+          <Route path='/' element={<Home />}>
+          </Route>
+        </Routes>
+      </Router>
+
+      
     </div>
   )
 }
