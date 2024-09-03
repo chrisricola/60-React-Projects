f385f9c42bc208bb90b37a7d7506c52c0a8b9e14
Chris Rico <chris.r.rico@gmail.com>
2024-09-03 16:20:18 -0700

    added two new files. A CheckoutProduct js and css file. Import CheckoutProduct into Checkout. Created layout for Check out of the right div and left div.

--- Ecommerce/src/Components/Checkout.js
+++ Ecommerce/src/Components/Checkout.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Checkout.css';
+import CheckoutProduct from './CheckoutProduct';
 
 const Checkout = () => {
   return (
-    <div>
-        <h1>Hello world</h1>
+    <div className='checkout'>
+        <div className="checkout__left">
+          <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" className="checkout__ad" />
+          <div>
+            <h2 className="checkout__title">Your Shopping Basket</h2>
+          </div>
+          <CheckoutProduct />
+        </div>
+        <div className="checkout__right"></div>
     </div>
   )
 }
