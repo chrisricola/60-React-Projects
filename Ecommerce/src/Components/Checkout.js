6a295999ba07d10266e42d4e5ac3c0cbd04be21e
Chris Rico <chris.r.rico@gmail.com>
2024-09-10 20:55:45 -0700

    moved CheckoutProduct in div. Styled CheckoutProduct page.

--- Ecommerce/src/Components/Checkout.js
+++ Ecommerce/src/Components/Checkout.js
@@ -10,8 +10,8 @@ const Checkout = () => {
           <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" className="checkout__ad" />
           <div>
             <h2 className="checkout__title">Your Shopping Basket</h2>
+            <CheckoutProduct />
           </div>
-          <CheckoutProduct />
         </div>
         <div className="checkout__right">
           <Subtotal />
